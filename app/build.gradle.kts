cb0fe77f0d7cc86b77b96254f7a933ab864c4372
Dinar <mingaleev.d92@mail.ru>
2025-01-18 09:27:41 +0300

    main exp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.example.sandbox_compose"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/sandbox_compose/MainActivity.kt
+++ app/src/main/java/com/example/sandbox_compose/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.sandbox_compose.ui.theme.Sandbox_composeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,18 +31,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApp()
-
+            Sandbox_composeTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                }
+            }
         }
-    }
-}
-
-@Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    Sandbox_composeTheme {
-        //        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        //
-        //        }
     }
 }
 
@@ -38,6 +43,8 @@ fun MyApp(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Sandbox_composeTheme {
-
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Text(text = "Hello")
+        }
     }
 }
